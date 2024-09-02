07a88eb3b5644e9fa1e1b1699ddad4260b0e82a7
MagatsuS <luqmanulhakimk3krk@gmail.com>
2024-09-02 21:13:07 +0800

    update vite confi

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Pokemon-App/",
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   build: {
     outDir: 'dist'  // Output folder
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
   }
 })
